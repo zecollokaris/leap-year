e7d377458aff1cce3413fdcf520992292703a578
zecollokaris <collo.kariss@gmail.com>
2018-02-25 23:20:17 +0300

    leap  year project

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
+  });
+
+
+  var leapYear = function(year) {
+      return false;
+    };
+
+    // jQuery user interface logic here.
+
+
+    var leapYear = function(year) {
+        if (year % 4 === 0) {
+          return true;
+        } else {
+          return false;
+        }
+      };
+
+      // jQuery user interface logic here.
+
+
+      var leapYear = function(year) {
+    if (year % 100 === 0) {
+      return false;
+    } else if (year % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // jQuery user interface logic here.
+
+
+  // business logic
+  var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // user interface logic
+  $(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+
+      $(".year").text(year);
+
+      if (!result) {                 // same as writing if (result === false)
+        $(".not").text("not");
+      } else {
+        $(".not").text("");
+      }
+
+      $("#result").show();
+    });
+  });
+  
